bdafb2fcb966d186dcd323fc78aa695e2e614b38
keerthana-256 <keerthanapasuparthi@gmail.com>
2026-02-28 20:06:25 +0530

    Added create-admin URL

--- exam/urls.py
+++ exam/urls.py
@@ -5,24 +5,17 @@ urlpatterns = [
 
     path('', views.invigilator_login, name='invigilator_login'),
     path('admin-login/', views.admin_login, name='admin_login'),
+    path('create-admin/', views.create_admin, name='create_admin'),
 
     path('invigilator/', views.invigilator_dashboard, name='invigilator_dashboard'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
 
-    path('take-attendance/<int:hall_id>/',
-         views.takeAttendance,
-         name='takeAttendance'),
-
+    path('take-attendance/<int:hall_id>/', views.takeAttendance, name='takeAttendance'),
     path('success/', views.success, name='success'),
-    path('create-admin/', views.create_admin, name='create_admin'),
 
-    path('section-absentees/<int:exam_id>/',
-         views.section_wise_absentees,
-         name='section_absentees'),
+    path('section-absentees/<int:exam_id>/', views.section_wise_absentees, name='section_absentees'),
 
-    path('export/<int:exam_id>/',
-         views.export_attendance_excel,
-         name='export_attendance_excel'),
+    path('export/<int:exam_id>/', views.export_attendance_excel, name='export_attendance_excel'),
 
     path('upload/', views.upload_students, name='upload_students'),
 ]
